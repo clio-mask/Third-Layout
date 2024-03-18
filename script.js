3b8df3b1d24960fe1f9267a0a1d470084373ede6
clio-mask <Amica.missa@gmail.com>
2024-03-18 21:51:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,14 +30,6 @@ window.addEventListener("DOMContentLoaded", () => {
     checker();
   };
 
-  const someFunc = (instance) => {
-    if (instance) {
-      instance.on("slideChange", function (e) {
-        console.log("*** mySwiper.activeIndex", instance.activeIndex);
-      });
-    }
-  };
-
   resizableSwiper("(max-width: 450px)", ".swiper", {
     loop: true,
     spaceBetween: -60,
